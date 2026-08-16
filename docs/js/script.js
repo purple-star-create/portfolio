d8c8344595ed0c8e875a2d7d1de28a9a0c0ceda0
purple-star-create <219644528+purple-star-create@users.noreply.github.com>
2026-08-16 17:43:46 +0200

    gifs page update + removal of front page gifs

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,7 +4,7 @@
 // Scripts exécutés quand le document est chargé (sauf images)
 $(document).ready(function(){
 
-	$(window).bind('scroll', function() {
+	/*$(window).bind('scroll', function() {
      if ($(window).scrollTop() > 200) {
          $('.duck, .cat').hide();
      }
